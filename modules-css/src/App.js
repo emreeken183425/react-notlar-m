85296bf804bc71a477e53a7eca21b54fa73a0c68
emreeken183425 <emreeken1834@gmail.com>
2022-06-23 22:08:18 +0300

    loading

--- modules-css/src/App.js
+++ modules-css/src/App.js
@@ -1,7 +1,15 @@
+import Card from './components/card/Card';
+import data from './util/data';
+
 function App() {
+  console.log(data);
   return (
     <>
-      <h1>APP</h1>
+      {data.map((item) => {
+        //!Destr
+        const { id, img, btnName, dil } = item;
+        return <Card key={id} img={img} btnName={btnName} dil={dil} />;
+      })}
     </>
   );
 }
--- sass/src/App.js
+++ sass/src/App.js
@@ -1,7 +1,8 @@
+import Card from './components/Card';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
-import Card from "./components/Card";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import './scss/App.scss';
 
 function App() {
   return (
