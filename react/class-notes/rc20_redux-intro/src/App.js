2bec4c1b7375a35600edaa0da41de2d1cdb9cf51
emreeken183425 <emreeken1834@gmail.com>
2022-07-22 23:19:32 +0300

    NEW

--- react/class-notes/rc20_redux-intro/src/App.js
+++ react/class-notes/rc20_redux-intro/src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
 import Counter from './components/counter/Counter';
 import Todo from './components/todo/Todo';
-import {createStore} from "redux";
-import {Provider} from "react-redux";
-import reducer from './redux';
-
-
 
 function App() {
-const store=createStore(reducer)
-
   return (
     <div className="app">
-      <Provider store={store}>
-          <Counter />
-          {/* <Todo /> */}
-      </Provider>
-    
-
+      <Counter />
+      <Todo />
     </div>
   );
 }
