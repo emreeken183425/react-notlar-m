7f44d144fb52044c8f033eb5c1bfa930f4c864e0
emreeken183425 <emreeken1834@gmail.com>
2022-07-23 19:54:00 +0300

    new

--- react/class-notes/rc20_redux-intro/src/App.js
+++ react/class-notes/rc20_redux-intro/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Counter from './components/counter/Counter';
 import Todo from './components/todo/Todo';
+// import { createStore } from 'redux';
+// import reducer from './redux';
+
+import { Provider } from 'react-redux';
+import { getStore } from './redux';
 
 function App() {
+  // const store = createStore(reducer);
+  const store = getStore();
   return (
     <div className="app">
-      <Counter />
-      <Todo />
+      <Provider store={store}>
+        {/* <Counter /> */}
+        <Todo />
+      </Provider>
     </div>
   );
 }
--- react/class-notes/rc20_redux-intro/src/redux/actions/counterActions.jsx
+++ react/class-notes/rc20_redux-intro/src/redux/actions/counterActions.jsx
@@ -1,7 +1,8 @@
-import { DECREMENT, INCREMENT, RESET } from "../types/counterTypes"
+import { DECREMENT, INCREMENT, RESET } from '../types/counterTypes';
 
-export const increment=()=>{
-    return{type:INCREMENT}
-}
-export const decrement=()=>({type:DECREMENT});
-export const reset=()=>({type:RESET});
+export const increment = () => {
+  return { type: INCREMENT };
+};
+
+export const decrement = () => ({ type: DECREMENT });
+export const reset = () => ({ type: RESET });
