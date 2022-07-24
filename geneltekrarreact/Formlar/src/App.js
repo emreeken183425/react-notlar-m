1123263d848269643fe5074ad39408814a0c30c0
emreeken183425 <emreeken1834@gmail.com>
2022-07-24 20:30:53 +0300

    new

--- geneltekrarreact/Formlar/src/App.js
+++ geneltekrarreact/Formlar/src/App.js
@@ -6,8 +6,8 @@ import MouseEvent from './components/3-mouseEvents/MouseEvent';
 function App() {
   return (
     <div className="container mt-4 text-center">
-      <Form />
-      {/* <FormObject/> */}
+      {/* <Form /> */}
+      <FormObject/>
       {/* <KeyboardEvents /> */}
       {/* <MouseEvent /> */}
     </div>
--- geneltekrarreact/Formlar/src/components/1-forms/FormObject.jsx
+++ geneltekrarreact/Formlar/src/components/1-forms/FormObject.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function FormObject() {
+const [username,setUsername]=useState('');
+const [email,setEmail]=useState('');
+const [password,setPassword]=useState('');
+
+const handleSubmit=()=>{
+alert(`username:${username}
+email:${email}
+password:${password}
+
+`)
+}
+
+
   return (
-    <div>FormObject</div>
+    <div>
+      <form onSubmit={handleSubmit} >
+      <div className="mb-3">
+    <label htmlFor="username" className="form-label">username:{username} </label>
+    <input type="text" className="form-control" id="username" onChange={(e)=>(setUsername(e.target.value) )} />
+  </div>
+  <div className="mb-3">
+    <label htmlFor="email" className="form-label">Email address:{email} </label>
+    <input type="email" className="form-control" id="email" onChange={(e)=>(setEmail(e.target.value))} />
+  </div>
+  <div className="mb-3">
+    <label htmlFor="password" className="Form-label">Password</label>
+    <input type="password" className="form-control" id="password" onChange={(e)=>(setPassword(e.target.value))} />
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+    </div>
   )
 }
 
