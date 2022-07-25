40c2e7ebb397a7245f188bc190cf43143e679d9a
emreeken183425 <emreeken1834@gmail.com>
2022-07-25 08:57:39 +0300

    vvvv

--- geneltekrarreact/Formlar/src/components/1-forms/FormObject.jsx
+++ geneltekrarreact/Formlar/src/components/1-forms/FormObject.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
 function FormObject() {
-const [username,setUsername]=useState('');
-const [email,setEmail]=useState('');
-const [password,setPassword]=useState('');
+const [formvalues,setFormValues]=useState({username:"",email:"",password:""})
+
 
 const handleSubmit=()=>{
+  const {username,password,email}=formvalues;
 alert(`username:${username}
 email:${email}
 password:${password}
@@ -13,21 +13,26 @@ password:${password}
 `)
 }
 
+const handFormValues=(e)=>{
+setFormValues({...formvalues,[e.target.id]:  e.target.value });
+
+}
+
 
   return (
     <div>
       <form onSubmit={handleSubmit} >
       <div className="mb-3">
-    <label htmlFor="username" className="form-label">username:{username} </label>
-    <input type="text" className="form-control" id="username" onChange={(e)=>(setUsername(e.target.value) )} />
+    <label htmlFor="username" className="form-label">username:{formvalues.username} </label>
+    <input type="text" className="form-control" id="username" onChange={handFormValues} />
   </div>
   <div className="mb-3">
-    <label htmlFor="email" className="form-label">Email address:{email} </label>
-    <input type="email" className="form-control" id="email" onChange={(e)=>(setEmail(e.target.value))} />
+    <label htmlFor="email" className="form-label">Email address:{formvalues.email} </label>
+    <input type="email" className="form-control" id="email" onChange={handFormValues} />
   </div>
   <div className="mb-3">
     <label htmlFor="password" className="Form-label">Password</label>
-    <input type="password" className="form-control" id="password" onChange={(e)=>(setPassword(e.target.value))} />
+    <input type="password" className="form-control" id="password" onChange={handFormValues} />
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
