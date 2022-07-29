ad480fd3bbfcd2d06d00d73c56128aae0a3a98bf
emreeken183425 <emreeken1834@gmail.com>
2022-07-29 21:18:30 +0300

    formik

--- react/class-notes/rc22_formik-yup/src/pages/LoginPage.jsx
+++ react/class-notes/rc22_formik-yup/src/pages/LoginPage.jsx
@@ -1,5 +1,81 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+import { FaLock } from 'react-icons/fa';
+import { Formik, Form } from 'formik';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Container maxWidth="sm" sx={{ mt: '10rem', textAlign: 'center' }}>
+      <Avatar
+        sx={{
+          backgroundColor: 'primary.main',
+          m: 'auto',
+          width: 60,
+          height: 60,
+        }}
+        sizes="100px"
+      >
+        <FaLock size="40" />
+      </Avatar>
+      <Typography variant="h4" align="center" mb={4} color="primary.dark">
+        Login
+      </Typography>
+
+      <Formik
+        initialValues={{ fullName: '', email: '', password: '' }}
+        onSubmit={(values, actions) => {
+          alert(`fullName: ${values.fullName}
+            email: ${values.email}
+            password: ${values.password}
+          `);
+          actions.resetForm();
+          actions.setSubmitting(false);
+        }}
+      >
+        {({ values, handleChange, errors, touched }) => (
+          <Form>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+              <TextField
+                label="Full Name"
+                name="fullName"
+                id="fullName"
+                type="type"
+                variant="outlined"
+                value={values.fullName}
+                onChange={handleChange}
+                helperText={touched.fullName && errors.fullName}
+                error={touched.fullName && Boolean(errors.fullName)}
+              />
+              <TextField
+                label="Email"
+                name="email"
+                id="email"
+                type="email"
+                variant="outlined"
+                value={values.email}
+                onChange={handleChange}
+              />
+              <TextField
+                label="password"
+                name="password"
+                id="password"
+                variant="outlined"
+                value={values.password}
+                onChange={handleChange}
+              />
+              <Button type="submit" variant="contained" size="large">
+                Submit
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 };
 
 export default LoginPage;
