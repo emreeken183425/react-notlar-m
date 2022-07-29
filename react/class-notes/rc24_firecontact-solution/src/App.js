38ec4a8edf8370e8d820a631b45728aabebe7b53
emreeken183425 <emreeken1834@gmail.com>
2022-07-29 10:56:55 +0300

    ek

--- react/class-notes/rc24_firecontact-solution/src/App.js
+++ react/class-notes/rc24_firecontact-solution/src/App.js
@@ -2,20 +2,37 @@ import './App.css';
 import Contacts from './components/contacts/Contacts';
 import FormComponent from './components/form/FormComponent';
 import {useState} from "react"
-import {AddUser} from "./utils/functions"
+import {AddUser,UpdateUser} from "./utils/functions"
+import {ToastContainer} from "react-toastify"
 
 const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
 
 function App() {
   const [info,setInfo]=useState(initialValues)
+  const [isAdd,setIsAdd]=useState("ADD")
+ 
+  
   const handleSubmit=(e)=>{
     e.preventDefault();
-    AddUser(info)
+    if(info.id){
+      UpdateUser(info)
+    }
+    else{
+      AddUser(info)
+    }
+    setInfo(initialValues);
+    setIsAdd("ADD")
+  }
+
+  const editUser=(id,username,phoneNumber,gender)=>{
+    setIsAdd("UPDATE")
+    setInfo({id,username,phoneNumber,gender})
   }
   return (
     <div className="App">
-      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} />
-      <Contacts/>
+      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} isAdd={isAdd} />
+      <Contacts editUser={editUser}/>
+      <ToastContainer/>
     </div>
   );
 }
